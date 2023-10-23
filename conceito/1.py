19c847f02f8f179ab389d7030032d6d666a69a4c
Isa <belagames012@gmail.com>
2023-10-23 07:54:13 -0300

    23/10/23

--- conceito/1.py
+++ conceito/1.py
@@ -1,3 +1,23 @@
-lista= [1, 0.5, 'PYTHON'] #- criar lista
+# criar listas
+    # lista = [1, 0.5, 'python']
+    # print (lista)
 
+# mostrar numeros da lista
+    # l = [10,20,30,40]
+    # for n in 1:
+        # print(n)
+
+# exibir os valores/elesmentos da lista
+    #for numeros in lista:
+        #print(numeros)
+
+# usuário acrescenta na lista
+    # indice 0, 1, 2, 3
+lista = [1,2,3]
 print(lista)
+# lista.append(int(input("Informe um inteiro: "))) # Acrescentar no final da lista
+numero = 2.5
+lista.insert(2,numero) # (indice do valor)  # Inserir o valor na posição desejada
+print(lista)
+
+# apagar um valor
---
+++ conceito/2.py
@@ -0,0 +1,7 @@
+quant = int(input("Informe a quantidade de números da lista: "))
+lista_n = []
+
+for c in range (quant):
+    numeros = int (input("Informe: "))
+    lista_n.append(numeros) #Salva os valores na lista
+print(lista_n)
